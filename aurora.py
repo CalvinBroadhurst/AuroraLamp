43cfd26aa33db572ecb1517596109e3e3f0016fe
Calvin Broadhurst <calvin.broadhurst@gmail.com>
2017-10-01 17:06:15 +1300

    Made threshold for notifications zero so it always sends one first time around

--- aurora.py
+++ aurora.py
@@ -198,15 +198,15 @@ def notifications(aurora_data):
 
     # Check to see what has changed and what we should be notifying about
     if aurora_data['s_g'] > aurora_data['last_s_g']:
-        message = message + 'G has increased to ' +  aurora_data['g'].text
-    if aurora_data['s_bz'] >= 3 and aurora_data['s_bz'] > aurora_data['last_s_bz']:
-        message = message + 'Bz has increased to ' +  aurora_data['bz'].text
-    if aurora_data['s_bt'] >= 3 and aurora_data['s_bt'] > aurora_data['last_s_bt']:
-        message = message + 'Bz has increased to ' +  aurora_data['bz'].text
-    if aurora_data['s_speed'] >= 3 and aurora_data['s_speed'] > aurora_data['last_s_speed']:
-        message = message + 'Speed has increased to ' +  aurora_data['speed'].text
-    if aurora_data['s_density'] >= 3 and aurora_data['s_density'] > aurora_data['last_s_density']:
-        message = message + 'Density has increased to ' +  aurora_data['density'].text
+        message = message + 'G has increased to ' + str(aurora_data['g']) + '\n'
+    if aurora_data['s_bz'] >= 0 and aurora_data['s_bz'] > aurora_data['last_s_bz']:
+        message = message + 'Bz has increased to ' +  str(aurora_data['bz']) + '\n'
+    if aurora_data['s_bt'] >= 0 and aurora_data['s_bt'] > aurora_data['last_s_bt']:
+        message = message + 'Bz has increased to ' +  str(aurora_data['bz']) + '\n'
+    if aurora_data['s_speed'] >= 0 and aurora_data['s_speed'] > aurora_data['last_s_speed']:
+        message = message + 'Speed has increased to ' +  str(aurora_data['speed']) + '\n'
+    if aurora_data['s_density'] >= 0 and aurora_data['s_density'] > aurora_data['last_s_density']:
+        message = message + 'Density has increased to ' +  str(aurora_data['density']) + '\n'
 
     # If we have a message to send then send it
     if message != '':
